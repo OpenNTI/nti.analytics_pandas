b3237d4961856c02a76623a7687a024edce2d9bc
Egawati Panjei <egawati.panjei@nextthought.com>
2015-08-13 08:31:05 +0000

    create connection to mysql and then build pandas dataframe
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@70475 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics_pandas/databases/db_connection.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id: db_connection.py 70252 2015-08-10 15:22:32Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from sqlalchemy import create_engine
+import pymysql
+from pandas import DataFrame
+from ..utils.string_utilities import StringFolder
+
+def create_engine_mysql(db_user='root', pwd=None, host='localhost', port='3306', db_name = 'Analytics'):
+	"""
+	create engine to access mysql db
+	"""
+	if pwd is None:
+		engine_string = u'mysql+pymysql://%s@%s:%s/%s' %(db_user, host, port, db_name)
+	else:
+		engine_string = u''
+	return create_engine(engine_string)
+
+
+def build_data_frame(engine, query):
+	with engine.connect() as connection:
+		# Execute the query against the database
+		results = (connection.execution_options(stream_results = True).execute(query))
+		#dataframe = DataFrame(iter(results))
+		dataframe = DataFrame(string_folding_wrapper(results))
+		dataframe.columns = results.keys()
+	connection.close()
+	return dataframe
+		
+
+def string_folding_wrapper(query_results):
+	"""
+	source : http://www.mobify.com/blog/sqlalchemy-memory-magic/
+	This generator yields rows from the results as tuples,
+	with all string values folded.
+	"""
+	# Get the list of keys so that we build tuples with all
+	# the values in key order.
+	keys = query_results.keys()
+	folder = StringFolder()
+	for row in query_results:
+		yield tuple(
+			folder.fold_string(row[key])
+			for key in keys
+		)
+
