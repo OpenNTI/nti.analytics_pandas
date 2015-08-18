d6b1a229611c948ee5562bf05fccdf96c7f3c57b
Egawati Panjei <egawati.panjei@nextthought.com>
2015-08-18 09:23:28 +0000

    add utility to transform orm query to dataframe
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@70825 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics_pandas/utils/orm_to_dataframe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id: orm_to_dataframe.py 70252 2015-08-10 15:22:32Z egawati.panjei $
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from pandas import DataFrame
+
+def orm_dataframe(orm_query, columns):
+	"""
+	takes sqlachemy orm query and a list of its columns and transform it to pandas dataframe
+	"""
+	def create_row(i):
+		return dict([(col, getattr(i, col))for col in columns])
+	return DataFrame([create_row(i) for i in orm_query])
