d2bda2a50287d3238f301c7147cae6c698e51baf
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-25 21:53:20 +0000

    formatting;whitespace
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@71682 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/utils/orm_to_dataframe.py
+++ src/nti/analytics_pandas/utils/orm_to_dataframe.py
@@ -3,6 +3,7 @@
 """
 .. $Id$
 """
+
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
@@ -14,9 +15,11 @@ def orm_dataframe(orm_query, columns):
 	"""
 	takes sqlachemy orm query and a list of its columns and transform it to pandas dataframe
 	"""
+
 	def create_row(i):
 		dictionary = {}
 		for col in columns:
 			if hasattr(i, col) : dictionary[col] = getattr(i, col)
 		return dictionary
+
 	return DataFrame([create_row(i) for i in orm_query])
