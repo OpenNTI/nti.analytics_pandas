cea2773007cac5630912f4b26749f059dc010773
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-08-25 21:51:41 +0000

    pymysql should be in the path
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@71681 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/databases/db_connection.py
+++ src/nti/analytics_pandas/databases/db_connection.py
@@ -9,31 +9,31 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from sqlalchemy import create_engine
-import pymysql
-from pandas import DataFrame
-from ..utils.string_utilities import StringFolder
 
-def create_engine_mysql(db_user='root', pwd=None, host='localhost', port='3306', db_name = 'Analytics'):
+from pandas import DataFrame
+
+from ..utils.string_folder import StringFolder
+
+def create_engine_mysql(db_user='root', pwd=None, host='localhost', port='3306',
+						db_name='Analytics'):
 	"""
 	create engine to access mysql db
 	"""
 	if pwd is None:
-		engine_string = u'mysql+pymysql://%s@%s:%s/%s' %(db_user, host, port, db_name)
+		engine_string = u'mysql+pymysql://%s@%s:%s/%s' % (db_user, host, port, db_name)
 	else:
-		engine_string = u'mysql+pymysql://%s:%s@%s:%s/%s' %(db_user, pwd, host, port, db_name)
+		engine_string = u'mysql+pymysql://%s:%s@%s:%s/%s' % (db_user, pwd, host, port, db_name)
 	return create_engine(engine_string)
-
 
 def build_data_frame(engine, query):
 	with engine.connect() as connection:
 		# Execute the query against the database
-		results = (connection.execution_options(stream_results = True).execute(query))
-		#dataframe = DataFrame(iter(results))
+		results = (connection.execution_options(stream_results=True).execute(query))
+		# dataframe = DataFrame(iter(results))
 		dataframe = DataFrame(string_folding_wrapper(results))
 		dataframe.columns = results.keys()
 	connection.close()
 	return dataframe
-		
 
 def string_folding_wrapper(query_results):
 	"""
@@ -47,7 +47,5 @@ def string_folding_wrapper(query_results):
 	folder = StringFolder()
 	for row in query_results:
 		yield tuple(
-			folder.fold_string(row[key])
-			for key in keys
+			folder.fold_string(row[key])for key in keys
 		)
-
