6f0be0f45e7edfeef06335ee123a7c981816ac54
Egawati Panjei <egawati.panjei@nextthought.com>
2015-09-04 10:12:56 +0000

    test enrollment query to dataframe
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@72344 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics_pandas/queries/tests/test_enrollments.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from hamcrest import equal_to
+from hamcrest import assert_that
+
+from nti.analytics_pandas.queries.enrollments import QueryCourseCatalogViews;
+from nti.analytics_pandas.queries.enrollments import QueryCourseEnrollments
+from nti.analytics_pandas.queries.enrollments import QueryCourseDrops
+from nti.analytics_pandas.queries.enrollments import QueryEnrollmentTypes
+
+from nti.analytics_pandas.tests import AnalyticsPandasTestBase
+
+class TestEnrollments(AnalyticsPandasTestBase):
+
+	def setUp(self):
+		super(TestEnrollments, self).setUp()
+
+	def test_query_course_catalog_views_by_period_of_time(self):
+		start_date = u'2015-03-01'
+		end_date = u'2015-05-31'
+		qccv = QueryCourseCatalogViews(self.session)
+		dataframe = qccv.filter_by_period_of_time(start_date, end_date)
+		assert_that(len(dataframe.index), equal_to(4880))
+
+	def test_query_course_enrollments_by_period_of_time(self):
+		start_date = u'2015-03-01'
+		end_date = u'2015-05-31'
+		qce = QueryCourseEnrollments(self.session)
+		dataframe = qce.filter_by_period_of_time(start_date, end_date)
+		assert_that(len(dataframe.index), equal_to(2718))
+
+	def test_query_course_drops_by_period_of_time(self):
+		start_date = u'2015-03-01'
+		end_date = u'2015-05-31'
+		qcd = QueryCourseDrops(self.session)
+		dataframe = qcd.filter_by_period_of_time(start_date, end_date)
+		assert_that(len(dataframe.index), equal_to(180))
+
+	def test_query_enrollment_types(self):
+		qet = QueryEnrollmentTypes(self.session)
+		dataframe = qet.get_enrollment_types()
+		assert_that(len(dataframe.index), equal_to(5))
+
+
+	
