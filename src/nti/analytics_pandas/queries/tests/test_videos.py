e845783855630c172fde9fcb14c4fd13a9246f2b
Egawati Panjei <egawati.panjei@nextthought.com>
2015-09-04 10:56:15 +0000

    create data frame for videos events and test it
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@72346 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics_pandas/queries/tests/test_videos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from hamcrest import equal_to
+from hamcrest import assert_that
+
+from nti.analytics_pandas.queries.videos import QueryVideoEvents
+
+from nti.analytics_pandas.tests import AnalyticsPandasTestBase
+
+class TestVideos(AnalyticsPandasTestBase):
+
+	def setUp(self):
+		super(TestVideos, self).setUp()
+
+	def test_query_course_catalog_views_by_period_of_time(self):
+		start_date = u'2015-03-01'
+		end_date = u'2015-05-31'
+		qve = QueryVideoEvents(self.session)
+		dataframe = qve.filter_by_period_of_time(start_date, end_date)
+		assert_that(len(dataframe.index), equal_to(105505))
---
+++ src/nti/analytics_pandas/queries/videos.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id: resource_views.py 71540 2015-08-24 16:41:40Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from nti.analytics.database.resource_views import VideoEvents
+
+from nti.common.property import Lazy
+
+from ..utils.orm_to_dataframe import orm_dataframe
+
+class VideosMixin(object):
+
+	table = None
+
+	def __init__(self, session):
+		self.session = session
+
+	@Lazy
+	def columns(self):
+		table = getattr(self.table, '__table__')
+		return table.columns.keys()
+
+class QueryVideoEvents(VideosMixin):
+	table = VideoEvents
+	def filter_by_period_of_time(self, start_date=None, end_date=None):
+		ve = self.table
+		query = self.session.query(	ve.video_view_id,
+									ve.timestamp,
+									ve.course_id,
+									ve.resource_id,
+									ve.context_path,
+									ve.time_length,
+									ve.video_event_type,
+									ve.video_start_time,
+									ve.video_end_time,
+									ve.with_transcript,
+									ve.session_id,
+									ve.user_id,
+									ve.play_speed).filter(ve.timestamp.between(start_date, end_date))
+		dataframe = orm_dataframe(query, self.columns)
+		return dataframe
+
