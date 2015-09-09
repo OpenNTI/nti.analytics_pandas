6cb091f118ea69e38024f995112a80c9d18979e3
Egawati Panjei <egawati.panjei@nextthought.com>
2015-09-09 08:11:05 +0000

    handling when dataframe is empty
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@72612 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/common_analysis_methods.py
+++ src/nti/analytics_pandas/analysis/common_analysis_methods.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-.. $Id: dataframe_operation.py 72535 2015-09-08 15:09:38Z carlos.sanchez $
+.. $Id: common_analysis_methods.py 72535 2015-09-08 15:09:38Z carlos.sanchez $
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -21,20 +21,23 @@ def add_timestamp_period_date(df):
 	return df
 
 def explore_number_of_events_based_timestamp_date_(df):
-	df = add_timestamp_period_date(df)
-	grouped = df.groupby('timestamp_period')
-	df.reset_index(inplace=True)
-	events_df = grouped.aggregate(pd.Series.nunique)
-	return events_df
+	if len(df.index) > 0 :
+		df = add_timestamp_period_date(df)
+		grouped = df.groupby('timestamp_period')
+		df.reset_index(inplace=True)
+		events_df = grouped.aggregate(pd.Series.nunique)
+		return events_df
 
 def explore_unique_users_based_timestamp_date_(df):
-	df = add_timestamp_period_date(df)
-	grouped = df.groupby('timestamp_period')
-	unique_users_per_period_df = grouped.agg({'user_id' : pd.Series.nunique})
-	unique_users_per_period_df.rename(columns={'user_id' : 'total_unique_users'}, inplace=True)
-	return unique_users_per_period_df
+	if len(df.index) > 0 :
+		df = add_timestamp_period_date(df)
+		grouped = df.groupby('timestamp_period')
+		unique_users_per_period_df = grouped.agg({'user_id' : pd.Series.nunique})
+		unique_users_per_period_df.rename(columns={'user_id' : 'total_unique_users'}, inplace=True)
+		return unique_users_per_period_df
 
 def explore_ratio_of_events_over_unique_users_based_timestamp_date_(events_df, events_df_column_name, unique_users_df):
-	merge_df = events_df.join(unique_users_df)
-	merge_df['ratio'] = merge_df[events_df_column_name] / merge_df['total_unique_users']
-	return merge_df
+	if events_df is not None and unique_users_df is not None:
+		merge_df = events_df.join(unique_users_df)
+		merge_df['ratio'] = merge_df[events_df_column_name] / merge_df['total_unique_users']
+		return merge_df
