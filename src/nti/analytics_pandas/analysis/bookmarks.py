a9c60c6d229ee4d21e34629a6392d8f1026988c9
Egawati Panjei <egawati.panjei@nextthought.com>
2015-09-14 10:29:31 +0000

    analyze bookmarks creation based on timestamp period, resource type and device type
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@72902 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/bookmarks.py
+++ src/nti/analytics_pandas/analysis/bookmarks.py
@@ -14,13 +14,17 @@ from ..queries import QueryBookmarksCreated
 from .common import explore_unique_users_based_timestamp_date_
 from .common import explore_number_of_events_based_timestamp_date_
 from .common import explore_ratio_of_events_over_unique_users_based_timestamp_date_
+from .common import analyze_types_
+from .common import add_timestamp_period
+
+import pandas as pd
 
 class BookmarkCreationTimeseries(object):
 	"""
 	analyze the number of bookmarks creation given time period and list of course id
 	"""
 
-	def __init__(self, session, start_date, end_date, course_id=None, with_resource_type=True, with_device_type=True):
+	def __init__(self, session, start_date, end_date, course_id=None, with_resource_type=True, with_device_type=True, time_period_date = True):
 		self.session = session
 		qbc = self.query_bookmarks_created = QueryBookmarksCreated(self.session)
 		if isinstance (course_id, (tuple, list)):
@@ -40,14 +44,20 @@ class BookmarkCreationTimeseries(object):
 			if new_df is not None: 
 				self.dataframe = new_df
 
+		if time_period_date :
+			self.dataframe = add_timestamp_period(self.dataframe)
+
 	def explore_number_of_events_based_timestamp_date(self):
 		events_df = explore_number_of_events_based_timestamp_date_(self.dataframe)
 		if events_df is not None :
 			events_df.rename(columns={'index':'total_bookmarks_created'}, inplace=True)
+		events_df = events_df[['total_bookmarks_created']]
+		print(events_df['total_bookmarks_created'].sum())
 		return events_df
 
 	def explore_unique_users_based_timestamp_date(self):
 		unique_users_per_period_df = explore_unique_users_based_timestamp_date_(self.dataframe)
+		print(unique_users_per_period_df['total_unique_users'].sum())
 		return unique_users_per_period_df
 
 	def explore_ratio_of_events_over_unique_users_based_timestamp_date(self):
@@ -56,3 +66,26 @@ class BookmarkCreationTimeseries(object):
 		merge_df = explore_ratio_of_events_over_unique_users_based_timestamp_date_(
 										events_df, 'total_bookmarks_created', unique_users_df)
 		return merge_df
+
+	def analyze_resource_types(self):
+		group_by_items = ['timestamp_period', 'resource_type']
+		agg_columns = {	'bookmark_id' 	: pd.Series.nunique,
+						'user_id'		: pd.Series.nunique,
+						'device_type'	: pd.Series.nunique}
+		df = analyze_types_(self.dataframe, group_by_items, agg_columns)
+		return df
+
+	def analyze_device_types(self):
+		group_by_items = ['timestamp_period', 'device_type']
+		agg_columns = {	'bookmark_id' 	: pd.Series.nunique,
+						'user_id'		: pd.Series.nunique }
+		df = analyze_types_(self.dataframe, group_by_items, agg_columns)
+		return df
+
+	def analyze_resource_device_types(self):
+		group_by_items = ['timestamp_period', 'resource_type', 'device_type']
+		agg_columns = {	'bookmark_id' 	: pd.Series.nunique,
+						'user_id'		: pd.Series.nunique	}
+		df = analyze_types_(self.dataframe, group_by_items, agg_columns)
+		return df
+
