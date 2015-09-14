3bba293279d4ae74a0283f19cf8f71f841289c6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-14 17:11:57 +0000

    should it be aggregate
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@72938 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/common.py
+++ src/nti/analytics_pandas/analysis/common.py
@@ -26,12 +26,12 @@ def explore_number_of_events_based_timestamp_date_(df):
 def explore_unique_users_based_timestamp_date_(df):
 	if len(df.index) > 0 :
 		grouped = df.groupby('timestamp_period')
-		unique_users_per_period_df = grouped.agg({'user_id' : pd.Series.nunique})
+		unique_users_per_period_df = grouped.aggregate({'user_id' : pd.Series.nunique})
 		unique_users_per_period_df.rename(columns={'user_id' : 'total_unique_users'}, inplace=True)
 		return unique_users_per_period_df
 
-def explore_ratio_of_events_over_unique_users_based_timestamp_date_(events_df, 
-																	events_df_column_name, 
+def explore_ratio_of_events_over_unique_users_based_timestamp_date_(events_df,
+																	events_df_column_name,
 																	unique_users_df):
 	if events_df is not None and unique_users_df is not None:
 		merge_df = events_df.join(unique_users_df)
