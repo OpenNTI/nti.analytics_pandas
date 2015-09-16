00c9e2b69d34dc1cfc7c352a56127604e4a56d92
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-16 21:54:44 +0000

    white space
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@73176 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/queries/bookmarks.py
+++ src/nti/analytics_pandas/queries/bookmarks.py
@@ -13,9 +13,10 @@ from nti.analytics.database.resource_tags import BookmarksCreated
 
 from .mixins import TableQueryMixin
 
-from . import orm_dataframe
 from .common import add_device_type_
 from .common import add_resource_type_
+
+from . import orm_dataframe
 
 class QueryBookmarksCreated(TableQueryMixin):
 
@@ -23,24 +24,24 @@ class QueryBookmarksCreated(TableQueryMixin):
 
 	def filter_by_period_of_time(self, start_date=None, end_date=None):
 		bc = self.table
-		query = self.session.query( bc.bookmark_id,
-									bc.timestamp,
-									bc.deleted,
-									bc.resource_id,
-									bc.session_id,
-									bc.user_id,
-									bc.course_id).filter(bc.timestamp.between(start_date, end_date))
+		query = self.session.query(bc.bookmark_id,
+								   bc.timestamp,
+								   bc.deleted,
+								   bc.resource_id,
+								   bc.session_id,
+								   bc.user_id,
+								   bc.course_id).filter(bc.timestamp.between(start_date, end_date))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
 	def filter_by_course_id_and_period_of_time(self, start_date=None, end_date=None, course_id=()):
 		bc = self.table
-		query = self.session.query( bc.bookmark_id,
-									bc.timestamp,
-									bc.deleted,
-									bc.resource_id,
-									bc.session_id,
-									bc.user_id).filter(bc.timestamp.between(start_date, end_date)).filter(bc.course_id.in_(course_id))
+		query = self.session.query(bc.bookmark_id,
+								   bc.timestamp,
+								   bc.deleted,
+								   bc.resource_id,
+								   bc.session_id,
+								   bc.user_id).filter(bc.timestamp.between(start_date, end_date)).filter(bc.course_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
--- src/nti/analytics_pandas/queries/resource_views.py
+++ src/nti/analytics_pandas/queries/resource_views.py
@@ -11,8 +11,8 @@ logger = __import__('logging').getLogger(__name__)
 
 from nti.analytics.database.resource_views import CourseResourceViews
 
-from .common import add_resource_type_
 from .common import add_device_type_
+from .common import add_resource_type_
 
 from .mixins import TableQueryMixin
 
@@ -24,26 +24,26 @@ class QueryCourseResourceViews(TableQueryMixin):
 
 	def filter_by_period_of_time(self, start_date=None, end_date=None):
 		crv = self.table
-		query = self.session.query( crv.resource_view_id,
-									crv.timestamp,
-									crv.course_id,
-									crv.resource_id,
-									crv.time_length,
-									crv.session_id,
-									crv.user_id,
-									crv.context_path).filter(crv.timestamp.between(start_date, end_date))
+		query = self.session.query(crv.resource_view_id,
+								   crv.timestamp,
+								   crv.course_id,
+								   crv.resource_id,
+								   crv.time_length,
+								   crv.session_id,
+								   crv.user_id,
+								   crv.context_path).filter(crv.timestamp.between(start_date, end_date))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
 	def filter_by_period_of_time_and_course_id(self, start_date=None, end_date=None, course_id=()):
 		crv = self.table
-		query = self.session.query( crv.resource_view_id,
-									crv.timestamp,
-									crv.resource_id,
-									crv.time_length,
-									crv.session_id,
-									crv.user_id,
-									crv.context_path).filter(crv.timestamp.between(start_date, end_date)).filter(crv.course_id.in_(course_id))
+		query = self.session.query(crv.resource_view_id,
+								   crv.timestamp,
+								   crv.resource_id,
+								   crv.time_length,
+								   crv.session_id,
+								   crv.user_id,
+								   crv.context_path).filter(crv.timestamp.between(start_date, end_date)).filter(crv.course_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
