677f6d87be3b69aecac2b39df5f3c6f5aac9b396
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-17 18:48:03 +0000

    check for get_values attribute
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@73213 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/utils/dataframe_operation.py
+++ src/nti/analytics_pandas/utils/dataframe_operation.py
@@ -9,19 +9,20 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import pandas
+import warnings
+
+try:
+	from pandas.core.categorical import Categorical
+except ImportError:
+	Categorical = type(None)
+	warnings.warn("pandas.core.categorical.Categorical not available")
 
 def get_values_of_series_categorical_index_(categorical_series):
 	index = categorical_series.index.values
-	if isinstance(index, pandas.core.categorical.Categorical):
+	if isinstance(index, Categorical) or hasattr(index, 'get_values'):
 		return index.get_values()
 
 def cast_columns_as_category_(df, list_of_columns):
 	for column in list_of_columns:
 		df[column] = df[column].astype('category')
 	return df
-
-
-
-
-
