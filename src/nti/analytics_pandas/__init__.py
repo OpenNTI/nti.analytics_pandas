d6b417c253ee10cc2d13df5851e470cb43db832c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-23 19:19:58 +0000

    change factory name
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@73526 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/__init__.py
+++ src/nti/analytics_pandas/__init__.py
@@ -10,5 +10,5 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory('nti.dataserver')
+MessageFactory = zope.i18nmessageid.MessageFactory('nti.analytics_pandas')
 
