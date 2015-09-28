bd22fcfd144722241681ad5eb7053fa9969df12d
Egawati Panjei <egawati.panjei@nextthought.com>
2015-09-28 05:59:44 +0000

    use ggplot to generate plot of resource views events
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@73699 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/resource_views.py
+++ src/nti/analytics_pandas/analysis/plots/resource_views.py
@@ -9,7 +9,9 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import pandas as pd
 import matplotlib.pyplot as plt
+from ggplot import *
 
 class  ResourceViewsTimeseriesPlot(object):
 
@@ -21,7 +23,7 @@ class  ResourceViewsTimeseriesPlot(object):
 
 	def explore_events(self):
 		"""
-		return line chart of the number of resource views during period of time
+		return scatter plots of resource views during period of time
 		it consists of :
 			- number of resource views
 			- number of unique users
@@ -29,6 +31,27 @@ class  ResourceViewsTimeseriesPlot(object):
 		"""
 		rvt = self.rvt
 		df = rvt.explore_ratio_of_events_over_unique_users_based_timestamp_date()
-		plt.figure()
-		figure = df.plot().get_figure()
-		return figure
+		df.reset_index(inplace=True)
+		df['timestamp_period'] = pd.to_datetime(df['timestamp_period'])
+		plot_resource_views = ggplot(df, aes(x='timestamp_period', y='total_resource_views')) + \
+					geom_point(color = 'orange') + \
+					ggtitle('Number of resource views during period of time') + \
+					scale_x_date(labels='%Y-%m-%d', breaks=date_breaks('1 week')) + \
+					ylab('Number of resource views') + \
+					xlab('Date')
+		plot_unique_users = ggplot(df, aes(x='timestamp_period', y='total_unique_users')) + \
+					geom_point(color = 'blue') + \
+					ggtitle('Number of unique users viewing resource during period of time') + \
+					scale_x_date(labels='%Y-%m-%d', breaks=date_breaks('1 week')) + \
+					ylab('Number of resource views') + \
+					xlab('Date')
+		plot_ratio = ggplot(df, aes(x='timestamp_period', y='ratio')) + \
+					geom_point(color = 'red') + \
+					ggtitle('Ratio of resource views over unique user on each available date') + \
+					scale_x_date(labels='%b%d', breaks=date_breaks('1 week')) + \
+					ylab('Ratio') + \
+					xlab('Date')
+		#ggsave(plot_resource_views, 'resource_views.png')
+		#ggsave(plot_unique_users, 'unique_users.png')
+		#ggsave(plot_ratio, 'ratio.png')
+		return plot_resource_views, plot_unique_users, plot_ratio
