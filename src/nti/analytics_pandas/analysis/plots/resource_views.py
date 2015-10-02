7b6b2ab33727f9f31129415172cd0ecc472281de
Egawati Panjei <egawati.panjei@nextthought.com>
2015-10-02 06:43:47 +0000

    should use resource type
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@73991 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/resource_views.py
+++ src/nti/analytics_pandas/analysis/plots/resource_views.py
@@ -165,30 +165,30 @@ class  ResourceViewsTimeseriesPlot(object):
 				ggplot(df, aes(x='timestamp_period', y='number_of_resource_views', color='resource_type')) + \
 				geom_point() + \
 				ggtitle('Number of resource views using each device type') + \
-				theme(title=element_text(size=10,face="bold")) + \
+				theme(title=element_text(size=8,face="bold")) + \
 				scale_x_date(breaks=period_breaks, minor_breaks=minor_period_breaks, labels=date_format("%y-%m-%d")) + \
 				ylab('Number of resource views') + \
 				xlab('Date') + \
 				facet_wrap('device_type', scales="free")
 
 		plot_unique_users = \
-				ggplot(df, aes(x='timestamp_period', y='number_of_unique_users', color='device_type')) + \
+				ggplot(df, aes(x='timestamp_period', y='number_of_unique_users', color='resource_type')) + \
 				geom_point() + \
 				ggtitle('Number of unique users viewing course resource given time period group by device type') + \
-				theme(title=element_text(size=10,face="bold")) + \
+				theme(title=element_text(size=8,face="bold")) + \
 				scale_x_date(breaks=period_breaks, minor_breaks=minor_period_breaks, labels=date_format("%y-%m-%d")) + \
 				ylab('Number of unique users') + \
 				xlab('Date') + \
 				facet_wrap('device_type', scales="free")
 
 		plot_unique_resources = \
-				ggplot(df, aes(x='timestamp_period', y='number_of_unique_resource', color='device_type')) + \
+				ggplot(df, aes(x='timestamp_period', y='number_of_unique_resource', color='resource_type')) + \
 				geom_point() + \
 				ggtitle('Number of unique course resource viewed on each device type at given time period') + \
-				theme(title=element_text(size=10,face="bold")) + \
+				theme(title=element_text(size=8,face="bold")) + \
 				scale_x_date(breaks=period_breaks, minor_breaks=minor_period_breaks, labels=date_format("%y-%m-%d")) + \
 				ylab('Number of unique course resource') + \
 				xlab('Date') + \
 				facet_wrap('device_type', scales="free")
-
+				
 		return (plot_resource_views, plot_unique_users, plot_unique_resources)
