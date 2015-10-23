cb81f9807ca9feebd9c0b82696b42615dc88c98b
Egawati Panjei <egawati.panjei@nextthought.com>
2015-10-23 17:55:04 +0000

    plot assignments taken
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@75390 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/assessments.py
+++ src/nti/analytics_pandas/analysis/plots/assessments.py
@@ -103,3 +103,88 @@ class AssignmentViewsTimeseriesPlot(object):
 				minor_breaks=minor_period_breaks)
 
 		return (plot_assignment_views, plot_unique_users, plot_ratio)
+
+class AssignmentsTakenTimeseriesPlot(object):
+
+	def __init__(self, att):
+		"""
+		att = AssignmentsTakenTimeseries
+		"""
+		self.att = att
+
+	def analyze_events(self, period_breaks='1 week', minor_period_breaks='1 day'):
+		att = self.att
+		df = att.analyze_events()
+		if df is None :
+			return ()
+		df.reset_index(inplace=True)
+		df['timestamp_period'] = pd.to_datetime(df['timestamp_period'])
+
+		plot_assignment_views = line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='number_assignments_taken',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Number of assignments viewed'),
+				title=_('Number of assignments taken during period of time'),
+				period_breaks=period_breaks,
+				minor_breaks=minor_period_breaks)
+
+		plot_unique_users = line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='number_of_unique_users',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Number of unique users'),
+				title=_('Number of unique users taking assignments during period of time'),
+				period_breaks=period_breaks,
+				minor_breaks=minor_period_breaks)
+
+		plot_ratio = line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='ratio',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Ratio'),
+				title=_('Ratio of assignments taken over unique user on each available date'),
+				period_breaks=period_breaks,
+				minor_breaks=minor_period_breaks)
+
+		return (plot_assignment_views, plot_unique_users, plot_ratio)
+
+	def analyze_events_group_by_device_type(self, period_breaks='1 week', minor_period_breaks='1 day'):
+		att = self.att
+		df = att.analyze_events_group_by_device_type()
+		if df is None :
+			return ()
+		df.reset_index(inplace=True)
+		df['timestamp_period'] = pd.to_datetime(df['timestamp_period'])
+
+		plot_assignment_views = group_line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='number_assignments_taken',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Number of assignments taken'),
+				title=_('Number of assignments taken grouped by device types during period of time'),
+				period_breaks=period_breaks,
+				group_by='device_type',
+				minor_breaks=minor_period_breaks)
+
+		plot_unique_users = group_line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='number_of_unique_users',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Number of unique users'),
+				title=_('Number of unique users taking assignments by device types during period of time'),
+				period_breaks=period_breaks,
+				group_by='device_type',
+				minor_breaks=minor_period_breaks)
+
+		plot_ratio = group_line_plot_x_axis_date(df=df,
+				x_axis_field='timestamp_period',
+				y_axis_field='ratio',
+				x_axis_label=_('Date'),
+				y_axis_label=_('Ratio'),
+				title=_('Ratio of assignments taken over unique user by device types on each available date'),
+				period_breaks=period_breaks,
+				group_by='device_type', 
+				minor_breaks=minor_period_breaks)
+
+		return (plot_assignment_views, plot_unique_users, plot_ratio)
--- src/nti/analytics_pandas/analysis/plots/tests/test_assessments.py
+++ src/nti/analytics_pandas/analysis/plots/tests/test_assessments.py
@@ -8,7 +8,9 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from nti.analytics_pandas.analysis.assessments import AssignmentViewsTimeseries
+from nti.analytics_pandas.analysis.assessments import AssignmentsTakenTimeseries
 from nti.analytics_pandas.analysis.plots.assessments import AssignmentViewsTimeseriesPlot
+from nti.analytics_pandas.analysis.plots.assessments import AssignmentsTakenTimeseriesPlot
 
 from nti.analytics_pandas.tests import AnalyticsPandasTestBase
 
@@ -33,3 +35,24 @@ class TestAssignmentViewsPlot(AnalyticsPandasTestBase):
 		avtp = AssignmentViewsTimeseriesPlot(avt)
 		_ = avtp.analyze_events_group_by_device_type(period_breaks='1 day', minor_period_breaks=None)
 
+
+class TestAssignmentsTakenPlot(AnalyticsPandasTestBase):
+
+	def setUp(self):
+		super(TestAssignmentsTakenPlot, self).setUp()
+
+	def test_analyze_events(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['1024', '1025', '1026', '1027', '1028']
+		att = AssignmentsTakenTimeseries(self.session, start_date=start_date, end_date=end_date, course_id=courses_id)
+		attp = AssignmentsTakenTimeseriesPlot(att)
+		_ = attp.analyze_events(period_breaks='1 week', minor_period_breaks='1 day')
+
+	def test_analyze_events_group_by_device_type(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['1024', '1025', '1026', '1027', '1028']
+		att = AssignmentsTakenTimeseries(self.session, start_date=start_date, end_date=end_date, course_id=courses_id)
+		attp = AssignmentsTakenTimeseriesPlot(att)
+		_ = attp.analyze_events_group_by_device_type(period_breaks='1 week', minor_period_breaks='1 day')
