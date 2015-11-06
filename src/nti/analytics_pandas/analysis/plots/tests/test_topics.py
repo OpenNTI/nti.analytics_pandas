79cf36198ae45530d5719ee1ab1b63b899ca4585
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-06 08:28:59 +0000

    plot topic favorites per course sections
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@76443 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/tests/test_topics.py
+++ src/nti/analytics_pandas/analysis/plots/tests/test_topics.py
@@ -136,7 +136,14 @@ class TestTopicFavoritesPlot(AnalyticsPandasTestBase):
 		tft = TopicFavoritesTimeseries(self.session, start_date, end_date, course_id)
 		tftp = TopicFavoritesTimeseriesPlot(tft)
 		_ = tftp.analyze_events_per_device_types(period_breaks='1 day', minor_period_breaks=None)
-		print(_)
+
+	def test_analyze_events_per_course_sections(self):
+		start_date = '2015-10-05'
+		end_date = '2015-10-27'
+		course_id = ['1068', '1096', '1097', '1098', '1099']
+		tft = TopicFavoritesTimeseries(self.session, start_date, end_date, course_id)
+		tftp = TopicFavoritesTimeseriesPlot(tft)
+		_ = tftp.analyze_events_per_course_sections(period_breaks='1 day', minor_period_breaks=None)
 
 class TestTopicsEventsPlot(AnalyticsPandasTestBase):
 
