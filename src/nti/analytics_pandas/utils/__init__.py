242ca76c13db8d8197aea7b360cfc4efe4df98f1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-12 17:07:43 +0000

    set props; pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@76899 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/utils/__init__.py
+++ src/nti/analytics_pandas/utils/__init__.py
@@ -9,8 +9,11 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from .save_plot import Plot
+
 from .string_folder import StringFolder
+
 from .orm_to_dataframe import orm_dataframe
+
 from .dataframe_operation import cast_columns_as_category_
 from .dataframe_operation import get_values_of_series_categorical_index_
-from .save_plot import Plot
--- src/nti/analytics_pandas/utils/save_plot.py
+++ src/nti/analytics_pandas/utils/save_plot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-.. $Id: save_plot.py 73738 2015-09-28 19:51:48Z carlos.sanchez $
+.. $Id$
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
@@ -10,9 +10,11 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from StringIO import StringIO
+
 import matplotlib.pyplot as plt
 
 class Image(object):
+
 	@classmethod
 	def process(cls, filename, data):
 		me = cls()
@@ -20,15 +22,8 @@ class Image(object):
 		me.data = data
 		return me
 
-def save_plot_to_png(plot, image_filename):
-	plt.figure.Figure = plot.draw()
-	buf = StringIO()
-	plt.savefig(buf, format='png')
-	plt.close()
-	image = Image.process(image_filename,buf)
-	return image
-
 class Plot(object):
+
 	@classmethod
 	def process(cls, plot_name, plot):
 		me = cls()
@@ -36,4 +31,10 @@ class Plot(object):
 		me.plot = plot
 		return me
 
-
+def save_plot_to_png(plot, image_filename):
+	plt.figure.Figure = plot.draw()
+	buf = StringIO()
+	plt.savefig(buf, format='png')
+	plt.close()
+	image = Image.process(image_filename, buf)
+	return image
