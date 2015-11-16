f253e28a3bc122255fa724d9c4eb675fc47c75b4
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-16 16:19:57 +0000

    use namedtemporaryfile, add TODO
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77158 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/views/commons.py
+++ src/nti/analytics_pandas/reports/views/commons.py
@@ -9,13 +9,27 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import matplotlib.pyplot as plt
+
 from ...utils import Plot
 from ...utils import save_plot_
 
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
+
 def build_plot_images_dictionary_(plots, image_type='png'):
+	#TODO : delete the named temporary files after using it to generate report
 	images = {}
 	for plot in plots:
 		if isinstance(plot, Plot):
 			image = save_plot_(plot.plot, plot.plot_name, image_type)
-			images[plot.plot_name] = image
+			image_file = NamedTemporaryFile(delete=False)
+			image.data.seek(0)
+			copyfileobj(image.data,image_file)
+			image.data.close()
+			images[plot.plot_name] = image_file.name
 	return images
+
+
+
+
