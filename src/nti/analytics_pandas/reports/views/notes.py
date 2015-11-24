9428f3609e3d6a7368941dee49e25ab7642ce923
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-24 10:06:00 +0000

    add note views per enrollment types to report
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77689 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/views/notes.py
+++ src/nti/analytics_pandas/reports/views/notes.py
@@ -217,6 +217,7 @@ class NoteEventsTimeseriesReportView(AbstractReportView):
 			self.options['has_note_views_data_per_course_sections'] = False
 		data = self.get_the_most_active_users_viewing_notes(data)
 		data = self.get_the_most_viewed_notes_and_its_author(data)
+		data = self.get_note_views_plots_per_enrollment_types(data)
 		return data
 
 	def get_note_views_plots(self, data):
@@ -235,6 +236,16 @@ class NoteEventsTimeseriesReportView(AbstractReportView):
 		if plots:
 			data['note_views_per_device_types'] = build_plot_images_dictionary(plots)
 			self.options['has_note_views_data_per_device_types'] = True
+		return data
+
+	def get_note_views_plots_per_enrollment_types(self, data):
+		plots = self.nvtp.analyze_total_events_based_on_enrollment_type(self.context.period_breaks,
+										 				  			self.context.minor_period_breaks,
+										 				  			self.context.theme_seaborn_)
+		self.options['has_note_views_data_per_enrollment_types'] = False
+		if plots:
+			data['note_views_per_enrollment_types'] = build_plot_images_dictionary(plots)
+			self.options['has_note_views_data_per_enrollment_types'] = True
 		return data
 
 	def get_note_views_plots_per_resource_types(self, data):
