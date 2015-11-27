49fb5287fa2335bb5ea78bef4e6d632cb3a77b6b
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-27 05:43:37 +0000

    report : add topics created per device types
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77828 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/views/topics.py
+++ src/nti/analytics_pandas/reports/views/topics.py
@@ -73,6 +73,7 @@ class TopicsTimeseriesReportView(AbstractReportView):
 	def generate_topics_created_plots(self, data):
 		self.tctp = TopicsCreationTimeseriesPlot(self.tct)
 		data = self.get_topics_created_plots(data)
+		data = self.get_topics_created_plots_per_device_types(data)
 		return data
 
 	def get_topics_created_plots(self, data):
@@ -81,6 +82,16 @@ class TopicsTimeseriesReportView(AbstractReportView):
 										 self.context.theme_seaborn_)
 		if plots :
 			data['topics_created'] = build_plot_images_dictionary(plots)
+		return data
+
+	def get_topics_created_plots_per_device_types(self, data):
+		plots = self.tctp.analyze_events_per_device_types(self.context.period_breaks,
+														  self.context.minor_period_breaks,
+														  self.context.theme_seaborn_)
+		self.options['has_topics_created_per_device_types'] = False
+		if plots :
+			data['topics_created_per_device_types'] = build_plot_images_dictionary(plots)
+			self.options['has_topics_created_per_device_types'] = True
 		return data
 
 View = TopicsTimeseriesReport = TopicsTimeseriesReportView
