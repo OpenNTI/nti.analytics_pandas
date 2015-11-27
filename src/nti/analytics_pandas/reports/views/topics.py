35ea821cdb883aa50c6e56635592f67798cfce2e
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-27 06:09:03 +0000

    report : add topics created per enrollment type
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77832 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/views/topics.py
+++ src/nti/analytics_pandas/reports/views/topics.py
@@ -73,6 +73,7 @@ class TopicsTimeseriesReportView(AbstractReportView):
 	def generate_topics_created_plots(self, data):
 		self.tctp = TopicsCreationTimeseriesPlot(self.tct)
 		data = self.get_topics_created_plots(data)
+		data = self.get_topics_created_plots_per_enrollment_types(data)
 		data = self.get_topics_created_plots_per_device_types(data)
 		if len(self.context.courses) > 1:
 			data = self.get_topics_created_plots_per_course_sections(data)
@@ -106,6 +107,16 @@ class TopicsTimeseriesReportView(AbstractReportView):
 		if plots :
 			data['topics_created_per_course_sections'] = build_images_dict_from_plot_dict(plots)
 			self.options['has_topics_created_per_course_sections'] = True
+		return data
+
+	def get_topics_created_plots_per_enrollment_types(self, data):
+		plots = self.tctp.analyze_events_per_enrollment_types(self.context.period_breaks,
+														  self.context.minor_period_breaks,
+														  self.context.theme_seaborn_)
+		self.options['has_topics_created_per_enrollment_types'] = False
+		if plots :
+			data['topics_created_per_enrollment_types'] = build_plot_images_dictionary(plots)
+			self.options['has_topics_created_per_enrollment_types'] = True
 		return data
 
 View = TopicsTimeseriesReport = TopicsTimeseriesReportView
