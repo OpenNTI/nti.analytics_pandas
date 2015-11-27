bbbb016a0ac72ff548c469443e15d58a5afecbc6
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-27 05:55:12 +0000

    report : add topics created per course sections
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77830 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/topics.py
+++ src/nti/analytics_pandas/analysis/plots/topics.py
@@ -122,7 +122,7 @@ class TopicsCreationTimeseriesPlot(object):
 		if df is None:
 			return ()
 
-		plots = []
+		plots = {}
 		df.reset_index(inplace=True)
 		df['timestamp_period'] = pd.to_datetime(df['timestamp_period'])
 		course_ids = np.unique(df['course_id'].values.ravel())
@@ -142,8 +142,9 @@ class TopicsCreationTimeseriesPlot(object):
 															 minor_period_breaks,
 															 theme_seaborn_,
 															 event_type)
-			plots.append(all_section_plots)
+			plots['all_section_plots'] = all_section_plots
 
+		section_plots_dict = {}
 		for course_id in course_ids:
 			new_df = df[df['course_id'] == course_id]
 			context_name = new_df.iloc[0]['context_name']
@@ -159,8 +160,9 @@ class TopicsCreationTimeseriesPlot(object):
 												minor_period_breaks,
 												theme_seaborn_,
 												event_type)
-			plots.append(section_plots)
-
+			key = 'section_%s' %(course_id)
+			section_plots_dict[key] = section_plots
+		plots['section_plots'] = section_plots_dict
 		return plots
 
 	def analyze_events_per_device_types(self, period_breaks='1 week',
