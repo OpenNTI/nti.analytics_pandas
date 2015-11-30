cd7d77bbb2b31e2672ab98619b744fc60ef47006
Egawati Panjei <egawati.panjei@nextthought.com>
2015-11-30 07:34:38 +0000

    check dataframe empty
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77871 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/bookmarks.py
+++ src/nti/analytics_pandas/analysis/bookmarks.py
@@ -40,36 +40,37 @@ class BookmarkCreationTimeseries(object):
 		else:
 			self.dataframe = qbc.filter_by_period_of_time(start_date, end_date)
 
-		categorical_columns = ['bookmark_id', 'user_id']
+		if not self.dataframe.empty:
+			categorical_columns = ['bookmark_id', 'user_id']
 
-		if with_resource_type:
-			new_df = qbc.add_resource_type(self.dataframe)
-			if new_df is not None:
-				self.dataframe = new_df
-				categorical_columns.append('resource_type')
+			if with_resource_type:
+				new_df = qbc.add_resource_type(self.dataframe)
+				if new_df is not None:
+					self.dataframe = new_df
+					categorical_columns.append('resource_type')
 
-		if with_device_type:
-			new_df = qbc.add_device_type(self.dataframe)
-			if new_df is not None:
-				self.dataframe = new_df
-				categorical_columns.append('device_type')
+			if with_device_type:
+				new_df = qbc.add_device_type(self.dataframe)
+				if new_df is not None:
+					self.dataframe = new_df
+					categorical_columns.append('device_type')
 
-		if with_context_name:
-			new_df = qbc.add_context_name(self.dataframe, course_id)
-			if new_df is not None:
-				self.dataframe = new_df
-				categorical_columns.append('context_name')
+			if with_context_name:
+				new_df = qbc.add_context_name(self.dataframe, course_id)
+				if new_df is not None:
+					self.dataframe = new_df
+					categorical_columns.append('context_name')
 
-		if with_enrollment_type:
-			new_df = qbc.add_enrollment_type(self.dataframe, course_id)
-			if new_df is not None:
-				self.dataframe = new_df
-				categorical_columns.append('enrollment_type')
+			if with_enrollment_type:
+				new_df = qbc.add_enrollment_type(self.dataframe, course_id)
+				if new_df is not None:
+					self.dataframe = new_df
+					categorical_columns.append('enrollment_type')
 
-		if time_period_date:
-			self.dataframe = add_timestamp_period_(self.dataframe)
+			if time_period_date:
+				self.dataframe = add_timestamp_period_(self.dataframe)
 
-		self.dataframe = cast_columns_as_category_(self.dataframe, categorical_columns)
+			self.dataframe = cast_columns_as_category_(self.dataframe, categorical_columns)
 
 	def analyze_events(self):
 		group_by_items = ['timestamp_period']
