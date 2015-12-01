f5896d48db4991d2e12356c98bfb52dac61216cf
Egawati Panjei <egawati.panjei@nextthought.com>
2015-12-01 14:42:46 +0000

    use common methods to generate plots for notes created
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@77988 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/notes.py
+++ src/nti/analytics_pandas/analysis/plots/notes.py
@@ -23,6 +23,9 @@ from .commons import line_plot_x_axis_date
 from .commons import group_line_plot_x_axis_date
 from .commons import histogram_plot_x_axis_discrete
 
+from .commons import generate_three_plots
+from .commons import generate_three_group_by_plots
+
 class NotesEventsTimeseriesPlot(object):
 
 	def __init__(self, net):
@@ -171,45 +174,19 @@ class NotesCreationTimeseriesPlot(object):
 					   period_breaks, minor_period_breaks, theme_seaborn_,
 					   event_type=None):
 
-		event_name, user_event_name, ratio_event_name = generate_plot_names(event_type)
-
-		plot_notes_created = line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='number_of_notes_created',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Number of notes created'),
-								title=event_title,
-								period_breaks=period_breaks,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=event_name)
-
-		plot_unique_users = line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='number_of_unique_users',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Number of unique users'),
-								title=user_title,
-								period_breaks=period_breaks,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=user_event_name)
-
-		plot_ratio = line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='ratio',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Ratio'),
-								title=ratio_title,
-								period_breaks=period_breaks,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=ratio_event_name)
-
-		return (plot_notes_created, plot_unique_users, plot_ratio)
+		event_y_axis_field = 'number_of_notes_created'
+		event_y_axis_label = _('Number of notes created')
+		plots = generate_three_plots(df,
+									 event_title,
+									 user_title,
+									 ratio_title,
+									 event_y_axis_field,
+									 event_y_axis_label,
+									 period_breaks,
+									 minor_period_breaks,
+									 theme_seaborn_,
+									 event_type)
+		return plots
 
 	def analyze_device_types(self, period_breaks='1 week', minor_period_breaks='1 day',
 							theme_seaborn_=True):
@@ -330,48 +307,21 @@ class NotesCreationTimeseriesPlot(object):
 		df.reset_index(inplace=True)
 		df['timestamp_period'] = pd.to_datetime(df['timestamp_period'])
 
-		event_name, user_event_name, ratio_event_name = generate_plot_names(event_type)
+		event_y_axis_field = 'number_of_notes_created'
+		event_y_axis_label = _('Number of notes created')
 
-		plot_notes_created = group_line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='number_of_notes_created',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Number of notes created'),
-								title=event_title,
-								period_breaks=period_breaks,
-								group_by=group_by,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=event_name)
-
-		plot_unique_users = group_line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='number_of_unique_users',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Number of unique users'),
-								title=user_title,
-								period_breaks=period_breaks,
-								group_by=group_by,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=user_event_name)
-
-		plot_ratio = group_line_plot_x_axis_date(
-								df=df,
-								x_axis_field='timestamp_period',
-								y_axis_field='ratio',
-								x_axis_label=_('Date'),
-								y_axis_label=_('Ratio'),
-								title=ratio_title,
-								period_breaks=period_breaks,
-								group_by=group_by,
-								minor_breaks=minor_period_breaks,
-								theme_seaborn_=theme_seaborn_,
-								plot_name=ratio_event_name)
-
-		return (plot_notes_created, plot_unique_users, plot_ratio)
+		plots = generate_three_group_by_plots(df,
+											  group_by,
+											  event_title,
+											  user_title,
+											  ratio_title,
+											  event_y_axis_field,
+											  event_y_axis_label,
+											  period_breaks,
+											  minor_period_breaks,
+											  theme_seaborn_,
+											  event_type)
+		return plots
 
 	def analyze_notes_created_on_videos(self, period_breaks='1 week',
 										minor_period_breaks='1 day',
