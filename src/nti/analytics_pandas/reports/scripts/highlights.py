b5ec0dd80aa122fc9f8c5999f144749f7c569eba
Egawati Panjei <egawati.panjei@nextthought.com>
2015-12-03 12:15:39 +0000

    add console script for highlights report
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@78204 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/analytics_pandas/reports/scripts/highlights.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id: highlights.py 77907 2015-11-30 16:28:37Z carlos.sanchez $
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import os
+
+from ..report import _configure_config
+from ..report import _setup_configs
+from ..report import process_args
+from ..report import Report
+
+from ..views import HighlightsTimeseriesContext
+from ..views import HighlightsTimeseriesReportView
+
+def main():
+	# Parse command line args
+	args = process_args()
+
+	_setup_configs()
+
+	_configure_config()
+	
+	# Create the output directory if it does not exist
+	if not os.path.exists(args['output']):
+		os.mkdir(args['output'])
+
+	filepath = '%s/highlights.pdf' %(args['output'])
+	
+	report_generator = Report(Context = HighlightsTimeseriesContext, 
+							  View = HighlightsTimeseriesReportView, 
+							  start_date = args['start_date'], 
+							  end_date = args['end_date'], 
+							  courses = args['courses'],
+						 	  period_breaks = args['period_breaks'], 
+						 	  minor_period_breaks = args['minor_period_breaks'],
+						 	  theme_seaborn_ = args['theme_seaborn'], 
+						 	  filepath=filepath)
+	report = report_generator.build()
+
+if __name__ == '__main__':  # pragma: no cover
+	main()
