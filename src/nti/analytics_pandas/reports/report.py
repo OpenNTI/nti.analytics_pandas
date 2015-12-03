ac8d96b688bd0d650a6ebbcc8a4054d4cc351f22
Egawati Panjei <egawati.panjei@nextthought.com>
2015-12-03 11:50:16 +0000

    ready to create console scripts for each report types
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@78198 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/report.py
+++ src/nti/analytics_pandas/reports/report.py
@@ -17,44 +17,31 @@ from zope.configuration import xmlconfig, config
 
 import nti.analytics_pandas
 
-from ..databases import DBConnection
-
-from .views import AssessmentsEventsTimeseriesContext
-from .views import AssessmentsEventsTimeseriesReportView
-
-from .views import BookmarksTimeseriesContext
-from .views import BookmarksTimeseriesReportView
-
-from .views import EnrollmentTimeseriesContext
-from .views import EnrollmentTimeseriesReportView
-
 from .views import create_pdf_file_from_rml
 from .views import cleanup_temporary_file
 
-from .views import ForumsTimeseriesContext
-from .views import ForumsTimeseriesReportView
-
-from .views import HighlightsTimeseriesContext
-from .views import HighlightsTimeseriesReportView
-
-from .views import NoteEventsTimeseriesContext
-from .views import NoteEventsTimeseriesReportView
-
-from .views import ResourceViewsTimeseriesContext
-from .views import ResourceViewsTimeseriesReportView
-
-from .views import TopicsTimeseriesContext
-from .views import TopicsTimeseriesReportView
-
-from .views import VideosTimeseriesContext
-from .views import VideosTimeseriesReportView
-
 from .z3c_zpt import ViewPageTemplateFile
+
+from ..databases import DBConnection
+
 
 DEFAULT_FORMAT_STRING = '[%(asctime)-15s] [%(name)s] %(levelname)s: %(message)s'
 
 def _parse_args():
 	arg_parser = argparse.ArgumentParser(description="NTI Analytics")
+	arg_parser.add_argument('start_date',
+							 help="Report duration start date, use the format %s" %'yyyy-mm-dd')
+	arg_parser.add_argument('end_date',
+							 help="Report duration end date, use the format %s" %'yyyy-mm-dd')
+	arg_parser.add_argument('courses',
+							 help="Course/s ID. For example %s" %'1068, 1096, 1097, 1098, 1099')
+	arg_parser.add_argument('-pb', '--period_breaks',
+							 help="Set period breaks on generated plots. The default is %s" %"'1 week'")
+	arg_parser.add_argument('-mpb', '--minor_period_breaks',
+							 help="Set minor period breaks on generated plots. The default is %s" %"'1 day'")
+	arg_parser.add_argument('-ts', '--theme_seaborn',
+							 default=True,
+							 help="Seaborn theme for generated plots. The default it is 1 (true)")
 	arg_parser.add_argument('-o', '--output',
 							 default='output',
 							 help="The output directory. The default is: %s" % 'output')
@@ -68,213 +55,63 @@ def _configure_logging(level='INFO'):
 def _setup_configs():
 	_configure_logging()
 
-def std_report_layout_rml():
-	path = os.path.join(os.path.dirname(__file__), 'templates/std_report_layout.rml')
-	return path
-
-def template(path):
-	result = ViewPageTemplateFile(path,
-								  auto_reload=(False,),
-								  debug=False)
-	return result
-
-def build_rml(Context, View, session, start_date, end_date, courses,
-			  period_breaks, minor_period_breaks, theme_seaborn_):
-
-	context = Context(session, start_date, end_date, courses,
-					  period_breaks, minor_period_breaks, theme_seaborn_)
-	view = View(context)
-	view()
-	path = std_report_layout_rml()
-	system = {'view':view, 'context':context}
-	rml = template(path).bind(view)(**system)
-	data = view.options['data']
-	return rml, data
-
-def generate_assessments_report(session, start_date, end_date, courses,
-			  					period_breaks, minor_period_breaks, theme_seaborn_,
-			  					output_dir):
-	Context = AssessmentsEventsTimeseriesContext
-	View = AssessmentsEventsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/assessments.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_bookmarks_report(session, start_date, end_date, courses,
-			  					period_breaks, minor_period_breaks, theme_seaborn_,
-			  					output_dir):
-	Context = BookmarksTimeseriesContext
-	View = BookmarksTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/bookmarks.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-
-def generate_enrollments_report(session, start_date, end_date, courses,
-			  					period_breaks, minor_period_breaks, theme_seaborn_,
-			  					output_dir):
-	Context = EnrollmentTimeseriesContext
-	View = EnrollmentTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/enrollments.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_forums_report(session, start_date, end_date, courses,
-			  			   period_breaks, minor_period_breaks, theme_seaborn_,
-			  			   output_dir):
-	Context = ForumsTimeseriesContext
-	View = ForumsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/forums.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_highlights_report(session, start_date, end_date, courses,
-				  			   period_breaks, minor_period_breaks, theme_seaborn_,
-				  			   output_dir):
-	Context = HighlightsTimeseriesContext
-	View = HighlightsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/highlights.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_notes_report(session, start_date, end_date, courses,
-			  			   period_breaks, minor_period_breaks, theme_seaborn_,
-			  			   output_dir):
-	Context = NoteEventsTimeseriesContext
-	View = NoteEventsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/notes.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_resource_views_report(session, start_date, end_date, courses,
-			  			   period_breaks, minor_period_breaks, theme_seaborn_,
-			  			   output_dir):
-	Context = ResourceViewsTimeseriesContext
-	View = ResourceViewsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/resource_views.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_topics_report(session, start_date, end_date, courses,
-			  			   period_breaks, minor_period_breaks, theme_seaborn_,
-			  			   output_dir):
-	Context = TopicsTimeseriesContext
-	View = TopicsTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/topics.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def generate_videos_report(session, start_date, end_date, courses,
-			  			   period_breaks, minor_period_breaks, theme_seaborn_,
-			  			   output_dir):
-	Context = VideosTimeseriesContext
-	View = VideosTimeseriesReportView
-	rml, data = build_rml(Context, View, session, start_date, end_date, courses,
-			  		period_breaks, minor_period_breaks, theme_seaborn_)
-
-	filepath = '%s/videos.pdf' % output_dir
-	report = create_pdf_file_from_rml(rml, filepath)
-	cleanup_temporary_file(data)
-	return report
-
-def main():
-	# Parse command line args
-	args = _parse_args()
-
-	_setup_configs()
-
-
+def _configure_config():
 	context = config.ConfigurationMachine()
 	xmlconfig.registerCommonDirectives(context)
 	xmlconfig.file('configure.zcml', package=nti.analytics_pandas, context=context)
 
-	# Create the output directory if it does not exist
-	if not os.path.exists(args.output):
-		os.mkdir(args.output)
+def str2bool(v):
+	return v.lower() in ("yes", "true", "t", "1")
 
-	db = DBConnection()
-
-	"""
-	only for test 
-	"""
-	start_date = '2015-10-05'
-	end_date = '2015-10-20'
-	courses = ['1068', '1096', '1097', '1098', '1099']
-	period_breaks = '1 day'
-	minor_period_breaks = None
-	theme_seaborn_ = True
-
-	"""
-	generate_bookmarks_report(db.session, start_date, end_date, courses,
-		  					  period_breaks, minor_period_breaks, theme_seaborn_,
-		  					  args.output)
-
-	generate_assessments_report(db.session, start_date, end_date, courses,
-			  					period_breaks, minor_period_breaks, theme_seaborn_,
-			  					args.output)
-
-	generate_enrollments_report(db.session, start_date, end_date, courses,
-			  					period_breaks, minor_period_breaks, theme_seaborn_,
-			  					args.output)
-
-	generate_forums_report(db.session, start_date, end_date, courses,
-		  				   period_breaks, minor_period_breaks, theme_seaborn_,
-		  				   args.output)
-
-	generate_highlights_report(db.session, start_date, end_date, courses,
-			  				   period_breaks, minor_period_breaks, theme_seaborn_,
-			  				   args.output)
-
-	generate_notes_report(db.session, start_date, end_date, courses,
-		  				  period_breaks, minor_period_breaks, theme_seaborn_,
-		  				  args.output)
-
-	generate_resource_views_report(db.session, start_date, end_date, courses,
-				  				   period_breaks, minor_period_breaks, theme_seaborn_,
-				  				   args.output)
-
-	generate_topics_report(db.session, start_date, end_date, courses,
-		  				  period_breaks, minor_period_breaks, theme_seaborn_,
-		  				  args.output)
-	"""
-	generate_videos_report(db.session, start_date, end_date, courses,
-		  				   period_breaks, minor_period_breaks, theme_seaborn_,
-		  				   args.output)
-
-	db.close_session()
+def process_args():
+	args = _parse_args()
+	args_dict = {}
+	args_dict['start_date'] = args.start_date 
+  	args_dict['end_date'] = args.end_date 
+  	
+  	if ',' in args.courses:
+  		args_dict['courses'] = args.courses.split(',')
+  	else:
+  		args_dict['courses'] = args.courses.split()
+	
+	args_dict['period_breaks'] = args.period_breaks
+	args_dict['minor_period_breaks'] = args.minor_period_breaks
+	
+	if isinstance(args.theme_seaborn, bool):
+		args_dict['theme_seaborn'] = args.theme_seaborn
+	else:
+		args_dict['theme_seaborn'] = str2bool(args.theme_seaborn)
+	
+	args_dict['output'] = args.output
+	return args_dict
 
 
-if __name__ == '__main__':  # pragma: no cover
-	main()
+class Report(object):
+	def __init__(self, Context, View, start_date, end_date, courses,
+				 period_breaks, minor_period_breaks,theme_seaborn_, filepath):
+		self.db = DBConnection()
+		self.context = Context(self.db.session, start_date, end_date, courses,
+					  		   period_breaks, minor_period_breaks, theme_seaborn_)
+		self.view = View(self.context)
+		self.filepath = filepath
+
+	def std_report_layout_rml(self):
+		path = os.path.join(os.path.dirname(__file__), 'templates/std_report_layout.rml')
+		return path
+
+	def template(self, path):
+		result = ViewPageTemplateFile(path,
+									  auto_reload=(False,),
+									  debug=False)
+		return result	
+	
+	def build(self):
+		self.view()
+		path = self.std_report_layout_rml()
+		system = {'view':self.view, 'context':self.context}
+		rml = self.template(path).bind(self.view)(**system)
+		data = self.view.options['data']
+		report = create_pdf_file_from_rml(rml, self.filepath)
+		cleanup_temporary_file(data)
+		self.db.session.close()
+		return report
