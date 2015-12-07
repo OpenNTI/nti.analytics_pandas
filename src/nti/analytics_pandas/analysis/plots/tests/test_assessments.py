be18f1b833e2e594e9e63fed717b92a8c352af98
Egawati Panjei <egawati.panjei@nextthought.com>
2015-12-07 03:35:34 +0000

    test assignment views when there is no data available
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@78406 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/plots/tests/test_assessments.py
+++ src/nti/analytics_pandas/analysis/plots/tests/test_assessments.py
@@ -7,6 +7,9 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import equal_to
+from hamcrest import assert_that
+
 from nti.analytics_pandas.analysis.assessments import AssignmentViewsTimeseries
 from nti.analytics_pandas.analysis.assessments import AssignmentsTakenTimeseries
 from nti.analytics_pandas.analysis.assessments import AssessmentEventsTimeseries
@@ -32,6 +35,16 @@ class TestAssignmentViewsPlot(AnalyticsPandasTestBase):
 		avtp = AssignmentViewsTimeseriesPlot(avt)
 		_ = avtp.analyze_events(period_breaks='1 day', minor_period_breaks=None)
 
+	def test_analyze_events_empty(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['xxx']
+		avt = AssignmentViewsTimeseries(self.session, start_date=start_date,
+										end_date=end_date, course_id=courses_id)
+		avtp = AssignmentViewsTimeseriesPlot(avt)
+		_ = avtp.analyze_events(period_breaks='1 day', minor_period_breaks=None)
+		assert_that(len(_), equal_to(0))
+
 	def test_analyze_events_group_by_device_type(self):
 		start_date = u'2015-01-01'
 		end_date = u'2015-05-31'
@@ -41,6 +54,17 @@ class TestAssignmentViewsPlot(AnalyticsPandasTestBase):
 		avtp = AssignmentViewsTimeseriesPlot(avt)
 		_ = avtp.analyze_events_group_by_device_type(period_breaks='1 day',
 													 minor_period_breaks=None)
+
+	def test_analyze_events_group_by_device_type_empty(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['xxx']
+		avt = AssignmentViewsTimeseries(self.session, start_date=start_date,
+										end_date=end_date, course_id=courses_id)
+		avtp = AssignmentViewsTimeseriesPlot(avt)
+		_ = avtp.analyze_events_group_by_device_type(period_breaks='1 day',
+													 minor_period_breaks=None)
+		assert_that(len(_), equal_to(0))
 
 	def test_analyze_events_group_by_enrollment_type(self):
 		start_date = u'2015-01-01'
@@ -52,6 +76,17 @@ class TestAssignmentViewsPlot(AnalyticsPandasTestBase):
 		_ = avtp.analyze_events_group_by_enrollment_type(period_breaks='1 day',
 														 minor_period_breaks=None)
 
+	def test_analyze_events_group_by_enrollment_type_empty(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['xxx']
+		avt = AssignmentViewsTimeseries(self.session, start_date=start_date,
+										end_date=end_date, course_id=courses_id)
+		avtp = AssignmentViewsTimeseriesPlot(avt)
+		_ = avtp.analyze_events_group_by_enrollment_type(period_breaks='1 day',
+														 minor_period_breaks=None)
+		assert_that(len(_), equal_to(0))
+
 	def test_analyze_events_per_course_sections(self):
 		start_date = u'2015-01-01'
 		end_date = u'2015-05-31'
@@ -61,6 +96,17 @@ class TestAssignmentViewsPlot(AnalyticsPandasTestBase):
 		avtp = AssignmentViewsTimeseriesPlot(avt)
 		_ = avtp.analyze_events_per_course_sections(period_breaks='1 day',
 													 minor_period_breaks=None)
+
+	def test_analyze_events_per_course_sections_empty(self):
+		start_date = u'2015-01-01'
+		end_date = u'2015-05-31'
+		courses_id = ['xxx']
+		avt = AssignmentViewsTimeseries(self.session, start_date=start_date,
+										end_date=end_date, course_id=courses_id)
+		avtp = AssignmentViewsTimeseriesPlot(avt)
+		_ = avtp.analyze_events_per_course_sections(period_breaks='1 day',
+													 minor_period_breaks=None)
+		assert_that(len(_), equal_to(0))
 
 class TestAssignmentsTakenPlot(AnalyticsPandasTestBase):
 
