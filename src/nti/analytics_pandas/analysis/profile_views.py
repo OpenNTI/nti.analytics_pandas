59ac5f663dd72ea93c122571f489469ef543bebc
Egawati Panjei <egawati.panjei@nextthought.com>
2015-12-18 07:57:05 +0000

    analyze profile activity views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@79165 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/analysis/profile_views.py
+++ src/nti/analytics_pandas/analysis/profile_views.py
@@ -13,6 +13,7 @@ import pandas as pd
 
 from ..queries import QueryUsers
 from ..queries import QueryEntityProfileViews
+from ..queries import QueryEntityProfileActivityViews
 
 from ..utils import cast_columns_as_category_
 from ..utils import get_values_of_series_categorical_index_
@@ -92,5 +93,78 @@ class EntityProfileViewsTimeseries(object):
 		most_viewed_profiles_df = target_df.merge(most_viewed_profiles_df)
 
 		most_viewed_profiles_df.sort_values(by='number_of_profile_viewed', ascending=[0], inplace=True)
+		most_viewed_profiles_df.reset_index(inplace=True, drop=True)
+		return most_viewed_profiles_df
+
+class EntityProfileActivityViewsTimeseries(object):
+	"""
+	analyze the profile views
+	"""
+
+	def __init__(self, session, start_date, end_date,
+				 with_application_type=True,
+				 period='daily',
+				 with_enrollment_type=True):
+		self.session = session
+		self.period = period
+		qepav = QueryEntityProfileActivityViews(self.session)
+
+		self.dataframe = qepav.filter_by_period_of_time(start_date, end_date)
+
+		if not self.dataframe.empty:
+			categorical_columns = ['target_id', 'user_id']
+			if with_application_type:
+				new_df = qepav.add_application_type(self.dataframe)
+				if new_df is not None:
+					self.dataframe = new_df
+
+			self.dataframe = add_timestamp_period_(self.dataframe, time_period=period)
+			self.dataframe = cast_columns_as_category_(self.dataframe, categorical_columns)
+
+	def analyze_events(self):
+		group_by_items = ['timestamp_period']
+		df = self.build_dataframe(group_by_items)
+		return df
+
+	def analyze_application_types(self):
+		group_by_items = ['timestamp_period', 'application_type']
+		df = self.build_dataframe(group_by_items)
+		return df
+
+	def build_dataframe(self, group_by_columns):
+		agg_columns = {	'target_id' 	: pd.Series.count,
+						'user_id'		: pd.Series.nunique }
+		df = analyze_types_(self.dataframe, group_by_columns, agg_columns)
+		if df is not None:
+			df.rename(columns={	'target_id'	:'number_of_profile_activity_views',
+								'user_id'	:'number_of_unique_users'},
+						inplace=True)
+			df['ratio'] = df['number_of_profile_activity_views'] / df['number_of_unique_users']
+			df = reset_dataframe_(df)
+		return df
+
+	def get_the_most_active_users(self, max_rank_number=10):
+		users_df = get_most_active_users_(self.dataframe, self.session, max_rank_number)
+		if users_df is not None:
+			users_df.rename(columns={'number_of_activities': 'number_of_profile_activity_views'},
+							inplace=True)
+		return users_df
+
+	def get_the_most_viewed_profile_activities(self, max_rank_number=10):
+		df = self.dataframe
+		if df is None or df.empty:
+			return
+		most_viewed_profiles_id = df.groupby('target_id').size().sort_values(ascending=False)[:max_rank_number]
+		most_viewed_profiles_df = most_viewed_profiles_id.to_frame(name='number_of_profile_activity_viewed')
+		most_viewed_profiles_df.reset_index(level=0, inplace=True)
+
+		target_id = get_values_of_series_categorical_index_(most_viewed_profiles_id).tolist()
+		target_df = QueryUsers(self.session).get_username_filter_by_user_id(target_id)
+		target_df.rename(columns={'user_id' : 'target_id', 'username': 'profile'},
+						 inplace=True)
+
+		most_viewed_profiles_df = target_df.merge(most_viewed_profiles_df)
+
+		most_viewed_profiles_df.sort_values(by='number_of_profile_activity_viewed', ascending=[0], inplace=True)
 		most_viewed_profiles_df.reset_index(inplace=True, drop=True)
 		return most_viewed_profiles_df
--- src/nti/analytics_pandas/analysis/tests/test_profile_views.py
+++ src/nti/analytics_pandas/analysis/tests/test_profile_views.py
@@ -12,6 +12,7 @@ from hamcrest import has_item
 from hamcrest import assert_that
 
 from nti.analytics_pandas.analysis.profile_views import EntityProfileViewsTimeseries
+from nti.analytics_pandas.analysis.profile_views import EntityProfileActivityViewsTimeseries
 
 from nti.analytics_pandas.tests import AnalyticsPandasTestBase
 
@@ -56,3 +57,45 @@ class TestEntityProfileViewsTimeseries(AnalyticsPandasTestBase):
 		epvt = EntityProfileViewsTimeseries(self.session, start_date, end_date)
 		df = epvt.get_the_most_viewed_profiles()
 		assert_that(df.columns, has_item('number_of_profile_viewed'))
+
+class TestEntityProfileActivityViewsTimeseries(AnalyticsPandasTestBase):
+
+	def test_analyze_events(self):
+		start_date = '2015-10-05'
+		end_date = '2015-10-19'
+		epavt = EntityProfileActivityViewsTimeseries(self.session, start_date, end_date)
+		assert_that(epavt.dataframe, has_item('timestamp'))
+		assert_that(epavt.dataframe, has_item('timestamp_period'))
+		assert_that(epavt.dataframe, has_item('target_id'))
+		assert_that(epavt.dataframe, has_item('user_id'))
+
+		df = epavt.analyze_events()
+		assert_that(df.empty, equal_to(False))
+		assert_that(df.columns, has_item('number_of_profile_activity_views'))
+		assert_that(df.columns, has_item('number_of_unique_users'))
+		assert_that(df.columns, has_item('ratio'))
+
+	def test_analyze_application_types(self):
+		start_date = '2015-10-05'
+		end_date = '2015-10-19'
+		epavt = EntityProfileActivityViewsTimeseries(self.session, start_date, end_date)
+		df = epavt.analyze_application_types()
+		assert_that(df.empty, equal_to(False))
+		assert_that(df.columns, has_item('application_type'))
+		assert_that(df.columns, has_item('number_of_profile_activity_views'))
+		assert_that(df.columns, has_item('number_of_unique_users'))
+		assert_that(df.columns, has_item('ratio'))
+
+	def test_get_the_most_active_users(self):
+		start_date = '2015-10-05'
+		end_date = '2015-10-19'
+		epavt = EntityProfileActivityViewsTimeseries(self.session, start_date, end_date)
+		df = epavt.get_the_most_active_users()
+		assert_that(df.columns, has_item('number_of_profile_activity_views'))
+
+	def test_get_the_most_viewed_profiles(self):
+		start_date = '2015-10-05'
+		end_date = '2015-10-19'
+		epavt = EntityProfileActivityViewsTimeseries(self.session, start_date, end_date)
+		df = epavt.get_the_most_viewed_profile_activities()
+		assert_that(df.columns, has_item('number_of_profile_activity_viewed'))
