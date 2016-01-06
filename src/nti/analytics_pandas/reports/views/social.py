1f2411c3fa8ae64b00abcc85b7e6367341dc0ddd
Egawati Panjei <egawati.panjei@nextthought.com>
2016-01-06 04:07:02 +0000

    add contacts removed plots into view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_pandas/trunk@79951 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_pandas/reports/views/social.py
+++ src/nti/analytics_pandas/reports/views/social.py
@@ -16,6 +16,9 @@ from zope import interface
 from ...analysis import ContactsAddedTimeseries
 from ...analysis import ContactsAddedTimeseriesPlot
 
+from ...analysis import ContactsRemovedTimeseries
+from ...analysis import ContactsRemovedTimeseriesPlot
+
 from .commons import build_plot_images_dictionary
 
 from .mixins import AbstractReportView
@@ -47,6 +50,8 @@ class SocialTimeseriesReportView(AbstractReportView):
 		keys = self.options.keys()
 		if 'has_contacts_added_data' not in keys:
 			self.options['has_contacts_added_data'] = False
+		if 'has_contacts_removed_data' not in keys:
+			self.options['has_contacts_removed_data'] = False
 		self.options['data'] = data
 		return self.options
 
@@ -62,6 +67,17 @@ class SocialTimeseriesReportView(AbstractReportView):
 		else:
 			self.options['has_contacts_added_data'] = True
 			data = self.generate_contacts_added_plots(data)
+
+		self.crt = ContactsRemovedTimeseries(self.context.session,
+										   	 self.context.start_date,
+										     self.context.end_date,
+										     period=self.context.period)
+		if self.crt.dataframe.empty:
+			self.options['has_contacts_removed_data'] = False
+		else:
+			self.options['has_contacts_removed_data'] = True
+			data = self.generate_contacts_removed_plots(data)
+
 		self._build_data(data)
 		return self.options
 
@@ -69,7 +85,7 @@ class SocialTimeseriesReportView(AbstractReportView):
 		self.catp = ContactsAddedTimeseriesPlot(self.cat)
 		data = self.get_contacts_added_plots(data)
 		data = self.get_contacts_added_plots_per_application_type(data)
-		data = self.get_the_most_active_users(data)
+		data = self.get_the_most_active_users_contacts_added(data)
 		return data
 
 	def get_contacts_added_plots(self, data):
@@ -91,13 +107,48 @@ class SocialTimeseriesReportView(AbstractReportView):
 			self.options['has_contacts_added_per_application_type'] = False
 		return data
 
-	def get_the_most_active_users(self, data):
+	def get_the_most_active_users_contacts_added(self, data):
 		plot = self.catp.plot_the_most_active_users(max_rank_number=self.context.number_of_most_active_user)
 		if plot:
 			data['contacts_added_users'] = build_plot_images_dictionary(plot)
 			self.options['has_contacts_added_users'] = True
 		else:
 			self.options['has_contacts_added_users'] = False
+		return data
+
+	def generate_contacts_removed_plots(self, data):
+		self.crtp = ContactsRemovedTimeseriesPlot(self.crt)
+		data = self.get_contacts_removed_plots(data)
+		data = self.get_contacts_removed_plots_per_application_type(data)
+		data = self.get_the_most_active_users_contacts_removed(data)
+		return data
+
+	def get_contacts_removed_plots(self, data):
+		plots = self.crtp.analyze_events(self.context.period_breaks,
+										 self.context.minor_period_breaks,
+										 self.context.theme_seaborn_)
+		if plots:
+			data['contacts_removed'] = build_plot_images_dictionary(plots)
+		return data
+
+	def get_contacts_removed_plots_per_application_type(self, data):
+		plots = self.crtp.analyze_application_types(self.context.period_breaks,
+													self.context.minor_period_breaks,
+													self.context.theme_seaborn_)
+		if plots:
+			data['contacts_removed_per_application_type'] = build_plot_images_dictionary(plots)
+			self.options['has_contacts_removed_per_application_type'] = True
+		else:
+			self.options['has_contacts_removed_per_application_type'] = False
+		return data
+
+	def get_the_most_active_users_contacts_removed(self, data):
+		plot = self.crtp.plot_the_most_active_users(max_rank_number=self.context.number_of_most_active_user)
+		if plot:
+			data['contacts_removed_users'] = build_plot_images_dictionary(plot)
+			self.options['has_contacts_removed_users'] = True
+		else:
+			self.options['has_contacts_removed_users'] = False
 		return data
 
 View = SocialTimeseriesReport = SocialTimeseriesReportView
