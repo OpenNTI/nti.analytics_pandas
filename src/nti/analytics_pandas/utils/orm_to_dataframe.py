925fd77347d67b29e85528810f8f178a86ce2673
Carlos Sanchez <papachoco@gmail.com>
2017-06-21 14:18:02 -0500

    require ggplot; pep8

--- src/nti/analytics_pandas/utils/orm_to_dataframe.py
+++ src/nti/analytics_pandas/utils/orm_to_dataframe.py
@@ -4,20 +4,23 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from pandas import DataFrame
 
+
 def create_row(obj, columns):
-	dictionary = {col:getattr(obj, col) for col in columns if hasattr(obj, col)}
-	return dictionary
+    dictionary = {col: getattr(obj, col)
+                  for col in columns if hasattr(obj, col)}
+    return dictionary
+
 
 def orm_dataframe(orm_query, columns):
-	"""
-	Takes sqlachemy orm query and a list of its columns and transform it to pandas dataframe
-	"""
-	result = DataFrame([create_row(i, columns) for i in orm_query])
-	return result
+    """
+    Takes sqlachemy orm query and a list of its columns and transform it to pandas dataframe
+    """
+    result = DataFrame([create_row(i, columns) for i in orm_query])
+    return result
