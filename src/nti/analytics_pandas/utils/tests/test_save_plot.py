107a90b5923f4b0ac228dd7b7a040fee5757fdc6
Carlos Sanchez <papachoco@gmail.com>
2017-11-30 16:12:46 -0600

    use 'Agg' for matplotlib

--- src/nti/analytics_pandas/utils/tests/test_save_plot.py
+++ src/nti/analytics_pandas/utils/tests/test_save_plot.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
+
+import matplotlib
+matplotlib.use('Agg')
 
 from ggplot import aes
 from ggplot import xlab
@@ -26,43 +30,46 @@ from nti.analytics_pandas.analysis.plots.resource_views import ResourceViewsTime
 
 from nti.analytics_pandas.tests import AnalyticsPandasTestBase
 
+
 def _build_testing_plot():
-	df = pd.DataFrame({
-		"x": np.arange(0, 100),
-		"y": np.arange(0, 100),
-		"z": np.arange(0, 100)
-	})
+    df = pd.DataFrame({
+        "x": np.arange(0, 100),
+        "y": np.arange(0, 100),
+        "z": np.arange(0, 100)
+    })
 
-	df['y'] = np.sin(df.y)
-	df['z'] = df['y'] + 100
-	df['c'] = np.where(df.x % 2 == 0, "red", "blue")
+    df['y'] = np.sin(df.y)
+    df['z'] = df['y'] + 100
+    df['c'] = np.where(df.x % 2 == 0, "red", "blue")
 
-	plot = ggplot(aes(x="x", color="c"), data=df)
-	plot = plot + geom_density() + xlab("x label") + ylab("y label")
-	return plot
+    plot = ggplot(aes(x="x", color="c"), data=df)
+    plot = plot + geom_density() + xlab("x label") + ylab("y label")
+    return plot
+
 
 class TestSavePlot(AnalyticsPandasTestBase):
 
-	def test_save_plot(self):
-		plot = _build_testing_plot()
-		image_filename = 'image_test.png'
-		image = save_plot_(plot, image_filename)
-		image.data.seek(0)
-		im = PLTImage.open(image.data)
-		im.show()
-		image.data.close()
+    def test_save_plot(self):
+        plot = _build_testing_plot()
+        image_filename = 'image_test.png'
+        image = save_plot_(plot, image_filename)
+        image.data.seek(0)
+        im = PLTImage.open(image.data)
+        im.show()
+        image.data.close()
 
-	def test_save_resource_views_plots(self):
-		start_date = '2015-01-01'
-		end_date = '2015-05-31'
-		course_id = ['1024']
-		rvt = ResourceViewsTimeseries(self.session, start_date, end_date, course_id)
-		rvtp = ResourceViewsTimeseriesPlot(rvt)
-		plots = rvtp.explore_events()
-		for plot in plots:
-			image_filename = u'%s' % (plot.plot_name)
-			image = save_plot_(plot.plot, image_filename, image_type='png')
-			image.data.seek(0)
-			im = PLTImage.open(image.data)
-			im.show()
-			image.data.close()
+    def test_save_resource_views_plots(self):
+        start_date = '2015-01-01'
+        end_date = '2015-05-31'
+        course_id = ['1024']
+        rvt = ResourceViewsTimeseries(
+            self.session, start_date, end_date, course_id)
+        rvtp = ResourceViewsTimeseriesPlot(rvt)
+        plots = rvtp.explore_events()
+        for plot in plots:
+            image_filename = u'%s' % (plot.plot_name)
+            image = save_plot_(plot.plot, image_filename, image_type='png')
+            image.data.seek(0)
+            im = PLTImage.open(image.data)
+            im.show()
+            image.data.close()
