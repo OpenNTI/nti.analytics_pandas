fa18b03d2bc5747447fadb91a9acf0c20f119a79
Egawati Panjei <egawati.panjei@gmail.com>
2018-01-10 18:04:31 +0700

    fix the failed test

--- src/nti/analytics_pandas/utils/tests/test_save_plot.py
+++ src/nti/analytics_pandas/utils/tests/test_save_plot.py
@@ -51,7 +51,8 @@ class TestSavePlot(AnalyticsPandasTestBase):
 
     def test_save_plot(self):
         plot = _build_testing_plot()
-        image_filename = 'image_test.png'
+        print(type(plot))
+        image_filename = 'image_test'
         image = save_plot_(plot, image_filename)
         image.data.seek(0)
         im = PLTImage.open(image.data)
