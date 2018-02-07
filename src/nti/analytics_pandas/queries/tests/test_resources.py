3d344ee44d6b01f6707b51f7d77928f0ef09402e
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-07 09:36:58 -0600

    pep8; pylint

--- src/nti/analytics_pandas/queries/tests/test_resources.py
+++ src/nti/analytics_pandas/queries/tests/test_resources.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import equal_to
 from hamcrest import assert_that
@@ -16,37 +16,36 @@ from nti.analytics_pandas.queries.resources import QueryResources
 
 from nti.analytics_pandas.tests import AnalyticsPandasTestBase
 
+
 class TestResources(AnalyticsPandasTestBase):
 
-	def setUp(self):
-		super(TestResources, self).setUp()
+    def test_get_all_resources(self):
+        qr = QueryResources(self.session)
+        dataframe = qr.get_all_resources()
+        assert_that(dataframe['resource_display_name'].iloc[0], 
+					equal_to('video'))
 
-	def test_get_all_resources(self):
-		qr = QueryResources(self.session)
-		dataframe = qr.get_all_resources()
-		assert_that(dataframe['resource_display_name'].iloc[0], equal_to('video'))
+    def test_get_resources_ds_id_given_id(self):
+        qr = QueryResources(self.session)
+        resources_id = (1,)
+        dataframe = qr.get_resources_ds_id_given_id(resources_id)
+        assert_that(len(dataframe.index), equal_to(1))
+        assert_that(dataframe.resource_id.iloc[0], equal_to(1))
 
-	def test_get_resources_ds_id_given_id(self):
-		qr = QueryResources(self.session)
-		resources_id = (1,)
-		dataframe = qr.get_resources_ds_id_given_id(resources_id)
-		assert_that(len(dataframe.index), equal_to(1))
-		assert_that(dataframe.resource_id.iloc[0], equal_to(1))
+    def test_get_resources_given_id(self):
+        qr = QueryResources(self.session)
+        resources_id = (1,)
+        dataframe = qr.get_resources_given_id(resources_id)
+        assert_that(len(dataframe.index), equal_to(1))
+        assert_that(dataframe.resource_id.iloc[0], equal_to(1))
+        assert_that(dataframe['max_time_length'].iloc[0], equal_to(500))
 
-	def test_get_resources_given_id(self):
-		qr = QueryResources(self.session)
-		resources_id = (1,)
-		dataframe = qr.get_resources_given_id(resources_id)
-		assert_that(len(dataframe.index), equal_to(1))
-		assert_that(dataframe.resource_id.iloc[0], equal_to(1))
-		assert_that(dataframe['max_time_length'].iloc[0], equal_to(500))
+    def test_add_resource_type(self):
+        qr = QueryResources(self.session)
+        dataframe = qr.get_all_resources()
+        dataframe = qr.add_resource_type(dataframe)
+        assert_that(dataframe['resource_type'].iloc[0], equal_to(u'video'))
 
-	def test_add_resource_type(self):
-		qr = QueryResources(self.session)
-		dataframe = qr.get_all_resources()
-		dataframe = qr.add_resource_type(dataframe)
-		assert_that(dataframe['resource_type'].iloc[0], equal_to(u'video'))
-
-		index = dataframe[dataframe['resource_id'] == np.int(1)].index.tolist()
-		idx = index[0]
-		assert_that(dataframe['resource_type'].iloc[idx], equal_to(u'video'))
+        index = dataframe[dataframe['resource_id'] == np.int(1)].index.tolist()
+        idx = index[0]
+        assert_that(dataframe['resource_type'].iloc[idx], equal_to(u'video'))
--- src/nti/analytics_pandas/queries/tests/test_sessions.py
+++ src/nti/analytics_pandas/queries/tests/test_sessions.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import equal_to
 from hamcrest import has_item
@@ -18,33 +18,34 @@ from nti.analytics_pandas.queries.sessions import QueryUserAgents
 
 from nti.analytics_pandas.tests import AnalyticsPandasTestBase
 
+
 class TestSessions(AnalyticsPandasTestBase):
 
-	def setUp(self):
-		super(TestSessions, self).setUp()
+    def test_sessions(self):
+        qs = QuerySessions(self.session)
+        sessions_id = [0, 1, 2, 3, 4, 5]
+        dataframe = qs.get_sessions_by_id(sessions_id)
+        assert_that(len(dataframe.index), 5)
+        assert_that(dataframe.columns, has_item('user_agent_id'))
 
-	def test_sessions(self):
-		qs = QuerySessions(self.session)
-		sessions_id = [0, 1, 2, 3, 4, 5]
-		dataframe = qs.get_sessions_by_id(sessions_id)
-		assert_that(len(dataframe.index), 5)
-		assert_that(dataframe.columns, has_item('user_agent_id'))
+    def test_user_agents(self):
+        qua = QueryUserAgents(self.session)
+        user_agents_id = [1]
+        dataframe = qua.get_user_agents_by_id(user_agents_id)
+        assert_that(len(dataframe.index), equal_to(1))
+        assert_that(dataframe.columns, has_item('user_agent_id'))
+        assert_that(dataframe.columns, has_item('user_agent'))
 
-	def test_user_agents(self):
-		qua = QueryUserAgents(self.session)
-		user_agents_id = [1]
-		dataframe = qua.get_user_agents_by_id(user_agents_id)
-		assert_that(len(dataframe.index), equal_to(1))
-		assert_that(dataframe.columns, has_item('user_agent_id'))
-		assert_that(dataframe.columns, has_item('user_agent'))
+    def test_user_agents_device(self):
+        qua = QueryUserAgents(self.session)
+        user_agents_id = [1]
 
-	def test_user_agents_device(self):
-		qua = QueryUserAgents(self.session)
-		user_agents_id = [1]
-		
-		dataframe = qua.get_user_agents_by_id(user_agents_id)
-		new_df = qua.add_device_type(dataframe)
-		assert_that(len(dataframe.index), equal_to(len(new_df.index)))
+        dataframe = qua.get_user_agents_by_id(user_agents_id)
+        new_df = qua.add_device_type(dataframe)
+        assert_that(len(dataframe.index), equal_to(len(new_df.index)))
 
-		index = dataframe[dataframe['user_agent_id'] == np.int(1)].index.tolist()
-		assert_that(dataframe['device_type'].iloc[index[0]], equal_to(u'Web App'))
+        index = dataframe[
+			dataframe['user_agent_id'] == np.int(1)
+		].index.tolist()
+        assert_that(dataframe['device_type'].iloc[index[0]],
+                    equal_to('Web App'))
