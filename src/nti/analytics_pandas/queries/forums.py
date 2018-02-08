e42fd5ceeea094e3439b1c7ca42ca16bfbc6717b
Egawati Panjei <egawati.panjei@gmail.com>
2018-02-08 18:39:24 +0700

    fix forums queries

--- src/nti/analytics_pandas/queries/forums.py
+++ src/nti/analytics_pandas/queries/forums.py
@@ -30,7 +30,7 @@ class QueryForumsCreated(TableQueryMixin):
 	def filter_by_period_of_time(self, start_date=None, end_date=None):
 		tc = self.table
 		query = self.session.query(tc.timestamp,
-								   tc.course_id,
+								   tc.root_context_id.label('course_id'),
 								   tc.forum_ds_id,
 								   tc.user_id,
 								   tc.session_id,
@@ -43,13 +43,13 @@ class QueryForumsCreated(TableQueryMixin):
 	def filter_by_period_of_time_and_course_id(self, start_date=None, end_date=None, course_id=()):
 		tc = self.table
 		query = self.session.query(tc.timestamp,
-								   tc.course_id,
+								   tc.root_context_id.label('course_id'),
 								   tc.forum_ds_id,
 								   tc.user_id,
 								   tc.session_id,
 								   tc.forum_id,
 								   tc.deleted,
-								   tc.entity_root_context_id).filter(tc.timestamp.between(start_date, end_date)).filter(tc.course_id.in_(course_id))
+								   tc.entity_root_context_id).filter(tc.timestamp.between(start_date, end_date)).filter(tc.root_context_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
@@ -65,7 +65,7 @@ class QueryForumsCommentsCreated(TableQueryMixin):
 	def filter_by_period_of_time(self, start_date=None, end_date=None):
 		fcc = self.table
 		query = self.session.query(fcc.timestamp,
-								   fcc.course_id,
+								   fcc.root_context_id.label('course_id'),
 								   fcc.user_id,
 								   fcc.topic_id,
 								   fcc.session_id,
@@ -96,7 +96,7 @@ class QueryForumsCommentsCreated(TableQueryMixin):
 								   fcc.parent_user_id,
 								   fcc.is_flagged,
 								   fcc.deleted,
-								   fcc.course_id).filter(fcc.timestamp.between(start_date, end_date)).filter(fcc.course_id.in_(course_id))
+								   fcc.root_context_id.label('course_id')).filter(fcc.timestamp.between(start_date, end_date)).filter(fcc.root_context_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
@@ -123,7 +123,7 @@ class QueryForumCommentFavorites(TableQueryMixin):
 								   fcf.user_id,
 								   fcf.comment_id,
 								   fcf.creator_id,
-								   fcf.course_id).filter(fcf.timestamp.between(start_date, end_date))
+								   fcf.root_context_id.label('course_id')).filter(fcf.timestamp.between(start_date, end_date))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
@@ -133,7 +133,8 @@ class QueryForumCommentFavorites(TableQueryMixin):
 								   fcf.session_id,
 								   fcf.user_id,
 								   fcf.comment_id,
-								   fcf.creator_id).filter(fcf.timestamp.between(start_date, end_date)).filter(fcf.course_id.in_(course_id))
+								   fcf.creator_id,
+								   fcf.root_context_id.label('course_id')).filter(fcf.timestamp.between(start_date, end_date)).filter(fcf.root_context_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
@@ -160,7 +161,7 @@ class QueryForumCommentLikes(TableQueryMixin):
 								   fcl.user_id,
 								   fcl.comment_id,
 								   fcl.creator_id,
-								   fcl.course_id).filter(fcl.timestamp.between(start_date, end_date))
+								   fcl.root_context_id.label('course_id')).filter(fcl.timestamp.between(start_date, end_date))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
@@ -171,7 +172,7 @@ class QueryForumCommentLikes(TableQueryMixin):
 								   fcl.user_id,
 								   fcl.comment_id,
 								   fcl.creator_id,
-								   fcl.course_id).filter(fcl.timestamp.between(start_date, end_date)).filter(fcl.course_id.in_(course_id))
+								   fcl.root_context_id.label('course_id')).filter(fcl.timestamp.between(start_date, end_date)).filter(fcl.root_context_id.in_(course_id))
 		dataframe = orm_dataframe(query, self.columns)
 		return dataframe
 
