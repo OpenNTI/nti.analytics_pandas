a97932d14ee157d69232a72356f6350ce54ce5e0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-02-20 14:51:15 -0600

    make sure all tables are created

--- src/nti/analytics_pandas/__init__.py
+++ src/nti/analytics_pandas/__init__.py
@@ -10,3 +10,7 @@ from __future__ import absolute_import
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory('nti.analytics_pandas')
+
+# Only a few of the tables appear in the metadata,
+# so we have to import some modules so they are known
+from nti.analytics_database.mime_types import FileMimeTypes
